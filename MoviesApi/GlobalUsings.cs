24784e3206837b6cfcc91d28b1b0550b2df12f9a
MalikBNB <167820077+MalikBNB@users.noreply.github.com>
2024-09-11 09:11:56 +0100

    Initial commit

---
+++ MoviesApi/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.OpenApi.Models;
+global using MoviesApi.Models;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using MoviesApi.Dtos;
+global using MoviesApi.Services;
+
---
+++ MoviesApi/Services/GenreService.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesApi.Services
+{
+    public class GenreService : IGenresService
+    {
+        private readonly AppDbContext _context;
+
+        public GenreService(AppDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<Genre> Add(Genre genre)
+        {
+            await _context.Genres.AddAsync(genre);
+            _context.SaveChanges();
+
+            return genre;
+        }
+
+        public Genre Delete(Genre genre)
+        {
+            _context.Remove(genre);
+            _context.SaveChanges();
+
+            return genre;
+        }
+
+        public async Task<IEnumerable<Genre>> GetAll()
+        {
+            return await _context.Genres.OrderBy(g => g.Name).ToListAsync();
+        }
+
+        public async Task<Genre> GetById(byte id)
+        {
+            return await _context.Genres.SingleOrDefaultAsync(g => g.Id == id);
+        }
+
+        public async Task<bool> IsValidGenre(byte id)
+        {
+            return await _context.Genres.AnyAsync(g => g.Id == id);
+        }
+
+        public Genre Update(Genre genre)
+        {
+            _context.Update(genre);
+            _context.SaveChanges();
+
+            return genre;
+        }
+    }
+}
